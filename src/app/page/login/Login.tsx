8c1794e17eeb6a7293bb71d014f744736caa9ed3
Luan-kaike <luangaymer828@gmail.com>
2023-07-11 09:05:33 -0300

    feat: show the erro on input on  login page

--- src/app/page/login/Login.tsx
+++ src/app/page/login/Login.tsx
@@ -11,32 +11,58 @@ export const Login = () => {
 
   const inputPassword = useRef<HTMLInputElement>(null)
   const inputId = useRef<HTMLInputElement>(null)
+  const spanPassword = useRef<HTMLSpanElement>(null)
+  const spanId = useRef<HTMLSpanElement>(null)
 
   const [id, setId] = useState('')
   const [password, setPassword] = useState('')
 
+  const verificationInputsIncorrect = useCallback(() => {
+    if(spanId.current && spanPassword.current){
+      spanId.current.innerHTML = '';
+      spanPassword.current.innerHTML = ''
+    
+      if(inputId.current){
+        if(!id.trim()){
+          spanId.current.innerHTML += 'Esse campo é obrigatório';
+          spanId.current.style.display = 'block';
+          inputId.current.className = 'inputIncorrect';
+        }
+        else if(!/^[a-zA-Z]$/.test(id[0])){
+          spanId.current.innerHTML = 'Esse campo não pode começar com números, caracteres especiais ou "Ç"';
+          spanId.current.style.display = 'block';
+          inputId.current.className = 'inputIncorrect';
+        }else {
+          inputId.current.className = '';
+          spanId.current.style.display = 'none';
+        }
+      }
+
+      if(inputPassword.current){
+        if(!password.trim()){
+          spanPassword.current.innerHTML = 'Esse campo é obrigatório';
+          spanPassword.current.style.display = 'block';
+          inputPassword.current.className = 'inputIncorrect';
+        }else {
+          inputPassword.current.className = '';
+          spanPassword.current.style.display = 'none';
+        }
+      }
+    }
+  }, [id, password])
+
   const verificationInputs = useCallback(() => {
     if(!id.trim() || !password.trim() || !/^[a-zA-Z]$/.test(id[0])){
-      if((!id.trim() || !/^[a-zA-Z]$/.test(id[0])) && inputId.current ) 
-      inputId.current.className = 'inputIncorrect';
-      else if(inputId.current) 
-      inputId.current.className = '';
-
-      console.log(id , password);
-
-      if(!password.trim() && inputPassword.current) 
-      inputPassword.current.className = 'inputIncorrect';
-      else if(inputPassword.current) 
-      inputPassword.current.className = '';
+      verificationInputsIncorrect()
       return;
     }
   
     console.log(id , password);
     logout(id)
     navigate('/pagina-inicial')
-  }, [id, password, navigate, logout]);
-
+  }, [id, password, logout, navigate, verificationInputsIncorrect]);
   
+
 
   return(
     <>
@@ -57,6 +83,7 @@ export const Login = () => {
               onKeyDown={(e) => e.key === 'Enter'? 
               inputPassword.current?.focus() : null}
             />
+            <span ref={spanId}></span>
           </label>
 
           <label>
@@ -67,6 +94,7 @@ export const Login = () => {
               onChange={(e) => setPassword(e.target.value)}
               onKeyDown={(e) =>{if(e.key === 'Enter'){verificationInputs()}}} 
             />
+            <span ref={spanPassword}></span>
           </label>
 
           <label>
@@ -78,6 +106,7 @@ export const Login = () => {
 
           <label><Link className="link" to={'/cadastro'}>Cadastrar</Link></label>
         </form>
+
       </div>
     </>
   );
